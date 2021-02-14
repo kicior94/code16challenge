b6673eed35d4842e9e133894b108965367b66010
kicior94 <tomekmarianski@gmail.com>
2021-02-14 11:22:08 +0100

    Code 16 challenge)

---
+++ code16challenge.py
@@ -0,0 +1,18 @@
+you_want_do_magic = True
+def ask_harry_ron_hermione():
+    print('Few important things, when it comes to magic')
+def what_is_important():
+    print('There are certain traits that need to be present in every team')
+def about_their_virtues():
+    [print(f'{k} was {v}') for k,v in {'Harry': 'courageous', 'Ron': 'loyal', 'Hermione': 'wise'}.items()]
+def about_their_values():
+    print('but...\nThey only succeded, because they were there for each other in the time of their need.\n It is impossible to overcome obstacles alone')
+def and_what_are_your_first_steps_to_become_a_wizard():
+    print('Cast a spell that transfers some money to someone in need.\nSee: https://www.code16challenge.pl')
+       
+if you_want_do_magic:
+    ask_harry_ron_hermione()
+    what_is_important()
+    about_their_virtues()
+    about_their_values()
+    and_what_are_your_first_steps_to_become_a_wizard()
